376cd0ec3d5da54bc63b36431112194a54d0f9e8
Leorevoir <leo.quinzler@epitech.eu>
2025-08-18 13:22:20 +0200

    feat: modular parser show

--- src/cplus/parser/parser_show.c
+++ src/cplus/parser/parser_show.c
@@ -5,16 +5,129 @@
 #include <error/assert.h>
 #include <stdio.h>
 
-void parser_show(const Parser *self);
+__cplus__used void parser_show(const Parser *self);
+__cplus__used static void ast_dump(const AST *n, int indent);
 
 /**
  * dump helpers
  */
 
+static const char *pad = "                                                                ";
+
 static __inline void sv_dump(const StrView s)
 {
     for (u64 i = 0; i < s.len; ++i)
         putchar(s.ptr[i]);
+}
+
+static __inline void ast_module_dump(const ASTModule *m, int indent)
+{
+    printf("%.*sModule\n", indent, pad);
+    for (size_t i = 0; i < m->items.len; ++i) {
+        ast_dump(m->items.data[i], indent + 2);
+    }
+}
+
+static __inline void ast_func_dump(const ASTFunc *f, int indent)
+{
+    printf("%.*sFunc ", indent, pad);
+    sv_dump(f->name);
+
+    printf("\n%.*s  params:\n", indent, pad);
+    for (u64 i = 0; i < f->params.len; ++i) {
+        ast_dump(f->params.data[i], indent + 4);
+    }
+
+    printf("%.*s  ret:\n", indent, pad);
+    ast_dump(f->ret_type, indent + 4);
+    printf("%.*s  body:\n", indent, pad);
+    ast_dump(f->body, indent + 4);
+}
+
+static __inline void ast_param_dump(const ASTParam *p, int indent)
+{
+    printf("%.*sParam ", indent, pad);
+    sv_dump(p->name);
+    printf(" : ");
+    ast_dump(p->type, 0);
+}
+
+static __inline void ast_type_dump(const ASTType *t, int indent)
+{
+    printf("%.*sType ", indent, pad);
+    sv_dump(t->name);
+    printf("\n");
+}
+
+static __inline void ast_block_dump(const ASTBlock *b, int indent)
+{
+    printf("%.*sBlock\n", indent, pad);
+    for (size_t i = 0; i < b->items.len; ++i) {
+        ast_dump(b->items.data[i], indent + 2);
+    }
+}
+
+static __inline void ast_return_dump(const ASTReturn *r, int indent)
+{
+    printf("%.*sReturn\n", indent, pad);
+    ast_dump(r->value, indent + 2);
+}
+
+static __inline void ast_let_dump(const ASTLet *l, int indent)
+{
+    printf("%.*sLet ", indent, pad);
+    sv_dump(l->name);
+    printf(" =\n");
+    ast_dump(l->value, indent + 2);
+}
+
+static __inline void ast_call_dump(const ASTCall *c, int indent)
+{
+    printf("%.*sCall ", indent, pad);
+    sv_dump(c->name);
+    printf("(\n");
+    for (size_t i = 0; i < c->args.len; ++i) {
+        ast_dump(c->args.data[i], indent + 2);
+    }
+    printf("%.*s)\n", indent, pad);
+}
+
+static __inline void ast_binop_dump(const ASTBinOp *b, int indent)
+{
+    printf("%.*sBinOp '", indent, pad);
+    sv_dump(b->op);
+    printf("'\n");
+    ast_dump(b->left, indent + 2);
+    ast_dump(b->right, indent + 2);
+}
+
+static __inline void ast_unop_dump(const ASTUnOp *u, int indent)
+{
+    printf("%.*sUnOp '", indent, pad);
+    sv_dump(u->op);
+    printf("'\n");
+    ast_dump(u->operand, indent + 2);
+}
+
+static __inline void ast_ident_dump(const ASTIdent *i, int indent)
+{
+    printf("%.*sIdent ", indent, pad);
+    sv_dump(i->name);
+    printf("\n");
+}
+
+static __inline void ast_number_dump(const ASTNumber *n, int indent)
+{
+    printf("%.*sNumber ", indent, pad);
+    sv_dump(n->value);
+    printf("\n");
+}
+
+static __inline void ast_string_dump(const ASTString *s, int indent)
+{
+    printf("%.*sString ", indent, pad);
+    sv_dump(s->value);
+    printf("\n");
 }
 
 static void ast_dump(const AST *n, int indent)
@@ -24,87 +137,60 @@ static void ast_dump(const AST *n, int indent)
         return;
     }
 
-    const char *pad = "                                                                ";
     switch (n->kind) {
+
         case AST_MODULE:
-            printf("%.*sModule\n", indent, pad);
-            for (size_t i = 0; i < n->module.items.len; ++i)
-                ast_dump(n->module.items.data[i], indent + 2);
+            ast_module_dump(&n->module, indent);
             break;
+
         case AST_FUNC:
-            printf("%.*sFunc ", indent, pad);
-            sv_dump(n->func.name);
-            printf("\n");
-            printf("%.*s  params:\n", indent, pad);
-            for (size_t i = 0; i < n->func.params.len; ++i)
-                ast_dump(n->func.params.data[i], indent + 4);
-            printf("%.*s  ret:\n", indent, pad);
-            ast_dump(n->func.ret_type, indent + 4);
-            printf("%.*s  body:\n", indent, pad);
-            ast_dump(n->func.body, indent + 4);
+            ast_func_dump(&n->func, indent);
             break;
+
         case AST_PARAM:
-            printf("%.*sParam ", indent, pad);
-            sv_dump(n->param.name);
-            printf(" : ");
-            ast_dump(n->param.type, 0);
+            ast_param_dump(&n->param, indent);
             break;
+
         case AST_TYPE:
-            printf("%.*sType ", indent, pad);
-            sv_dump(n->type.name);
-            printf("\n");
+            ast_type_dump(&n->type, indent);
             break;
+
         case AST_BLOCK:
-            printf("%.*sBlock\n", indent, pad);
-            for (size_t i = 0; i < n->block.items.len; ++i)
-                ast_dump(n->block.items.data[i], indent + 2);
+            ast_block_dump(&n->block, indent);
             break;
+
         case AST_RETURN:
-            printf("%.*sReturn\n", indent, pad);
-            ast_dump(n->ret.value, indent + 2);
+            ast_return_dump(&n->ret, indent);
             break;
+
         case AST_LET:
-            printf("%.*sLet ", indent, pad);
-            sv_dump(n->let.name);
-            printf(" =\n");
-            ast_dump(n->let.value, indent + 2);
+            ast_let_dump(&n->let, indent);
             break;
+
         case AST_CALL:
-            printf("%.*sCall ", indent, pad);
-            sv_dump(n->call.name);
-            printf("(\n");
-            for (size_t i = 0; i < n->call.args.len; ++i)
-                ast_dump(n->call.args.data[i], indent + 2);
-            printf("%.*s)\n", indent, pad);
+            ast_call_dump(&n->call, indent);
             break;
+
         case AST_BINOP:
-            printf("%.*sBinOp '", indent, pad);
-            sv_dump(n->binop.op);
-            printf("'\n");
-            ast_dump(n->binop.left, indent + 2);
-            ast_dump(n->binop.right, indent + 2);
+            ast_binop_dump(&n->binop, indent);
             break;
+
         case AST_UNOP:
-            printf("%.*sUnOp '", indent, pad);
-            sv_dump(n->unop.op);
-            printf("'\n");
-            ast_dump(n->unop.operand, indent + 2);
+            ast_unop_dump(&n->unop, indent);
             break;
+
         case AST_IDENT:
-            printf("%.*sIdent ", indent, pad);
-            sv_dump(n->ident.name);
-            printf("\n");
+            ast_ident_dump(&n->ident, indent);
             break;
+
         case AST_NUMBER:
-            printf("%.*sNumber ", indent, pad);
-            sv_dump(n->number.value);
-            printf("\n");
+            ast_number_dump(&n->number, indent);
             break;
+
         case AST_STRING:
-            printf("%.*sString ", indent, pad);
-            sv_dump(n->string.value);
-            printf("\n");
+            ast_string_dump(&n->string, indent);
             break;
+
         default:
             __assert(false, "Unknown AST kind: %d", n->kind);
     }
