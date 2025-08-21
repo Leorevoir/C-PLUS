772f5d76879fecc11a797f1b41980969ce04b51b
Leorevoir <leo.quinzler@epitech.eu>
2025-08-21 17:22:32 +0200

    feat: add semantic analysis finally working

--- src/cpa/semantic/analyse_functions.c
+++ src/cpa/semantic/analyse_functions.c
@@ -7,8 +7,13 @@ void analyse_function(CPASemanticAnalysis *c, const AST *node)
 
     __assert(decl, "function must be declared");
     __assert(decl->param_count == func->params.len, "internal error param count mismatch");
+
+    /** set current return type & push a new local scope */
+    CPlusTypeInfo prev_ret = c->current_ret;
+    c->current_ret = decl->return_type;
     _push_scope(c);
 
+    /** bind parameters in the new top scope */
     for (u64 i = 0; i < func->params.len; ++i) {
         const AST *params = func->params.data[i];
         __assert(params->kind == AST_PARAM, "Expected AST_PARAM in function parameters");
@@ -16,7 +21,11 @@ void analyse_function(CPASemanticAnalysis *c, const AST *node)
         _define_type_table(c, params->param.name, type_info, params->line, params->column);
     }
 
+    /** check body */
     __assert(func->body && func->body->kind == AST_BLOCK, "Function body must be a block");
     analyse_statement(c, func->body);
+
+    /** pop scope & restore previous return type */
     _pop_scope(c);
+    c->current_ret = prev_ret;
 }
